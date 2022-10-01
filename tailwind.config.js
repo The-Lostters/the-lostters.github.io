4942b4107307844103536152220c77e6d1d1dcc7
Joe Song <jsong@songmn.com>
2022-10-01 14:57:26 -0500

    Simpler build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.html'],
-  theme: {},
-  variants: {},
+  content: [],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-};
+}
